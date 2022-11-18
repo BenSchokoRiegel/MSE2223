e0ecd19cec4348c23555cae486fde442895b3b06
Your Name <you@example.com>
2022-11-18 18:50:40 +0100

    übung 1 Gitter

--- app/src/main/java/com/example/mse2223/MainActivity.kt
+++ app/src/main/java/com/example/mse2223/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.mse2223
 
+import android.content.ClipData
 import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -21,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mse2223.ui.theme.MSE2223Theme
 import com.example.mse2223.ui.theme.Note
+import com.example.mse2223.ui.theme.NotePriority
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,16 +46,51 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Rasterlayout(notes : List<Note> ){
-    Column {
-        notes.forEach { note ->
-            create_Colum(note = note)
-        }
-    }
+fun Rasterlayout(notes : List<Note> ) {
+    LazyVerticalGrid(cells = GridCells.Fixed(3),verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        items(notes) { note ->
 
+            var background = Color.Black;
+
+            if (note.priority == NotePriority.DEFAULT){
+                background = Color.Yellow;
+            } else if (note.priority == NotePriority.ARCHIVED){
+                background = Color.Yellow;
+            } else {
+                background = Color.Yellow;
+            }
+
+            Card {
+                Column() {
+                    Row(){
+                        Text(note.title)
+                    }
+                    Row(){
+                        if(note.text != ""){
+                            Text(note.text)
+                        }
+                    }
+                    Row(){
+                        Text(note.priority.toString())
+                    }
+
+
+
+                }
+
+            }
+        }
+
+
+    }
 }
+
+
+
+
 
 @Composable
 fun create_Colum(note : Note, modifier: Modifier = Modifier){
@@ -90,4 +130,8 @@ fun DefaultPreview() {
     MSE2223Theme {
         Greeting("Android")
     }
+}
+
+fun createRow(note: Note) {
+
 }
