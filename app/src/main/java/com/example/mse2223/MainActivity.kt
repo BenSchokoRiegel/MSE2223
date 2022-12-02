c126004954b828cf82a8a1f5065863607cac78fb
ben <ben.riegel@mni.thm.de>
2022-12-02 14:57:16 +0100

    Aufgaben erledigt

--- app/src/main/java/com/example/mse2223/MainActivity.kt
+++ app/src/main/java/com/example/mse2223/MainActivity.kt
@@ -1,38 +1,48 @@
 package com.example.mse2223
 
 import android.content.ClipData
+import android.graphics.fonts.Font
 import androidx.compose.ui.graphics.Color
 import android.os.Bundle
+import android.text.style.StyleSpan
+import android.text.SpannableString
+import android.text.TextDirectionHeuristic
+
 import android.view.RoundedCorner
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+import androidx.compose.ui.text.font.Typeface
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mse2223.ui.theme.MSE2223Theme
 import com.example.mse2223.ui.theme.Note
 import com.example.mse2223.ui.theme.NotePriority
+import com.example.mse2223.ui.theme.getStringWithFix
+import java.util.Locale.filter
+import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val note = Note("Bannanen");
-        val note2 = Note("apfel")
-        val note3 = Note("Schokolade", "250g")
-        val liste: List<Note> = listOf(note, note2, note3)
+
+        val liste: List<Note> = getList(50)
 
         super.onCreate(savedInstanceState)
         setContent {
@@ -46,49 +56,103 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Composable
+fun Coloums(notes : List<Note>){
+
+    List<List<Notes>>
+
+        LazyRow() {
+
+
+        }
+    }
+
+}
+
+
+
+
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Rasterlayout(notes : List<Note> ) {
-    LazyVerticalGrid(cells = GridCells.Fixed(3),verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-        items(notes) { note ->
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(3), verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+
+    ) {
+        items((notes.filter{note -> note.priority != NotePriority.ARCHIVED})) { note ->
 
             var background = Color.Black;
 
-            if (note.priority == NotePriority.DEFAULT){
+            if (note.priority == NotePriority.DEFAULT) {
                 background = Color.Yellow;
-            } else if (note.priority == NotePriority.ARCHIVED){
-                background = Color.Yellow;
+            } else if (note.priority == NotePriority.ARCHIVED) {
+                background = Color.Green;
+
             } else {
-                background = Color.Yellow;
+                background = Color(250, 0, 50);
+
             }
 
-            Card {
-                Column() {
-                    Row(){
+            Card() {
+                Column(
+                    modifier = Modifier
+                        .background(background)
+                        .fillMaxHeight()
+                        .fillMaxWidth()
+                ) {
+                    Row() {
                         Text(note.title)
                     }
-                    Row(){
-                        if(note.text != ""){
-                            Text(note.text)
-                        }
+                    Row() {
+                        Text(note.text)
                     }
-                    Row(){
-                        Text(note.priority.toString())
-                    }
+                    Row() {
 
+
+                        Text(
+                            text = note.priority.getStringWithFix(),
+                            fontWeight = FontWeight.Bold,
+                            overflow = TextOverflow.Ellipsis
+                        )
+
+                    }
 
 
                 }
 
             }
         }
-
-
     }
+
 }
 
+fun getList(length: Int):List<Note> {
+    val note = Note("Bannanen");
+    val note2 = Note("apfel")
+    val note3 = Note("Schokolade", "250g")
+    val tomate = Note("tomate", "250g")
+    val  erdbere = Note("erg","st", NotePriority.ARCHIVED)
+    val liste: List<Note> = listOf(note, note2, note3,tomate,erdbere)
 
+
+    val res: MutableList<Note> = mutableListOf()
+    val list = listOf("Bannanen", "apfel", "Schokolade","tomate","Erbeere", "Kiwi","Birne")
+
+
+    for(i in 1..length){
+
+        val enum = NotePriority.values().get(Random.nextInt(0, 3))
+        val weight =   Random.nextInt(0, 2000).toString() + "g"
+        val ele =  list.get(Random.nextInt(0,list.size))
+        res.add(Note(ele,weight,enum))
+    }
+
+    return res
+
+
+}
 
 
 
--- app/src/main/java/com/example/mse2223/Note.kt
+++ app/src/main/java/com/example/mse2223/Note.kt
@@ -3,13 +3,38 @@ package com.example.mse2223.ui.theme
 enum class NotePriority {
     IMPORTANT, DEFAULT, ARCHIVED
 }
+
+fun NotePriority.toString(): String{
+    if (this.equals(NotePriority.IMPORTANT)){
+        return "IMPORTANT" + "!!!!"
+    } else {
+        return this.name + "s"
+    }
+}
+
+
+fun NotePriority.getStringWithFix() : String{
+    if (this.equals(NotePriority.IMPORTANT)){
+        return (this.toString() + "!!!!")
+    } else {
+        return this.toString()
+    }
+}
 /**
 class Note constructor(val title: String, val text: String, val priority: NotePriority) {
     constructor(title: String) : this(title, "", NotePriority.DEFAULT)
     constructor(title: String, text: String) : this(title, text, NotePriority.DEFAULT)
 }
 */
-class Note (val title: String, val text: String, val priority: NotePriority) {
-    constructor(title: String) : this(title, "", NotePriority.DEFAULT)
-    constructor(title: String, text: String) : this(title, text, NotePriority.DEFAULT)
+class Note (val title: String, val text: String, val priority: NotePriority, val hasPicture : Boolean) {
+
+
+
+    constructor(title: String) : this(title, "", NotePriority.DEFAULT,false)
+    constructor(title: String, text: String) : this(title, text, NotePriority.DEFAULT,false)
+    constructor(title: String, text: String,priority: NotePriority) : this(title, text, priority,false)
+    //constructor(title: String, text: String,priorit) : this(title, text, NotePriority.DEFAULT)
+
+
+
 }
